f6b1af985a7c9fdf698c65c7362d8db7a0ac2956
Eduard <eduard.bibac@protonmail.com>
2024-04-09 23:59:51 +0300

    chore: Remove unused components

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
 import {useTranslations} from 'next-intl';
-import LangSwitcher from "@/components/ui/LangSwitcher";
-import Link from "next/link";
 import DesktopNavigation from "@/components/ui/DesktopNavigation";
-
 
 export default function Home() {
   const t = useTranslations('Index');
