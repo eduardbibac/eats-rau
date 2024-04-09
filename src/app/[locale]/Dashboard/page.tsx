4f691cf28cc51b3b0157bcca03bed6c952dc8dcc
Eduard <eduard.bibac@protonmail.com>
2024-04-10 00:01:00 +0300

    feat: Add new Dashboard page

---
+++ src/app/[locale]/Dashboard/page.tsx
@@ -0,0 +1,9 @@
+import DesktopNavigation from "@/components/ui/DesktopNavigation";
+
+export default function Dashboard() {
+    return (
+      <>
+      <DesktopNavigation></DesktopNavigation>
+      </>
+    );
+}
