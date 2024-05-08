890d592766470a0077bd49d84b86e7d92acebd0b
Eduard <eduard.bibac@protonmail.com>
2024-05-08 09:24:59 +0300

    ui: product grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import BottomNavBar from "@/components/BottomNavBar";
 import Navbar from "@/components/NavBar";
 import Image from "next/image";
+import Shop from "@/components/Shop";
 
 export default function Home() {
   return (
     <>
     <Navbar></Navbar>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <Shop></Shop>
+    {/* <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -112,7 +114,7 @@ export default function Home() {
           </p>
         </a>
       </div>
-    </main>
+    </main> */}
     <BottomNavBar></BottomNavBar>
     </>
   );
