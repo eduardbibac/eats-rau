31bf360649b1e7d9dda092de9912badab1942af4
Eduard <eduard.bibac@protonmail.com>
2024-05-08 16:25:56 +0300

    ui: shop layout

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,5 +1,14 @@
-export default function Shop() {
+import BottomNavBar from "@/components/BottomNavBar";
+import CartPage from "@/components/CartPage";
+import Navbar from "@/components/NavBar";
+import Shop from "@/components/Shop";
+import "@/styles/shop-page.css";
+
+export default function ShopPage() {
     return (
-        <></>
+<>
+    <Shop></Shop>
+    <CartPage></CartPage>
+</>
     );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,8 @@
 //https://preline.co/examples/navigations-navbars.html
 
 import Link from "next/link";
+import SearchBar from "@/components/SearchBar";
+import "@/styles/navbar.css";
 
 export default function Navbar () {
     return(
@@ -22,14 +24,14 @@ export default function Navbar () {
     </div>
 
     {/* <!-- Button Group --> */}
+    <SearchBar></SearchBar>
     <div className="flex items-center gap-x-2 ms-auto py-1 md:ps-6 md:order-3 md:col-span-3">
-      <Link href="/login" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl border border-gray-200 text-black hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none">
+      {/* <Link href="/login" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl border border-gray-200 text-black hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none">
         Sign in
-      </Link>
-      <button type="button" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl border border-transparent bg-lime-400 text-black hover:bg-lime-500 transition disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-lime-500">
-        Hire us
-      </button>
-
+      </Link> */}
+      <Link href="/login" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl border border-transparent bg-lime-400 text-black hover:bg-lime-500 transition disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-lime-500">
+        Sign in
+        </Link>
       <div className="md:hidden">
         <button type="button" className="hs-collapse-toggle size-[38px] flex justify-center items-center text-sm font-semibold rounded-xl border border-gray-200 text-black hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none" data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
           <svg className="hs-collapse-open:hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
@@ -37,11 +39,13 @@ export default function Navbar () {
         </button>
       </div>
     </div>
+    
+    
     {/* <!-- End Button Group --> */}
 
     {/* <!-- Collapse --> */}
     <div id="navbar-collapse-with-animation" className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block md:w-auto md:basis-auto md:order-2 md:col-span-6">
-      <div className="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:justify-center md:items-center md:gap-y-0 md:gap-x-7 md:mt-0">
+      <div className="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:justify-center md:items-center md:gap-y-0 md:gap-x-7 md:mt-0">    
         <div>
           <a className="relative inline-block text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400" href="#" aria-current="page">Home</a>
         </div>
@@ -59,6 +63,7 @@ export default function Navbar () {
         </div> */}
       </div>
     </div>
+
     {/* <!-- End Collapse --> */}
   </nav>
 </header>
