303be7dfcc8aad158a59d47fcc931f76ecd66e82
Eduard <eduard.bibac@protonmail.com>
2024-05-12 01:03:45 +0300

    ui: button and layout improvements

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -2,7 +2,20 @@ import "@/styles/shop-page.css";
 import Navbar from "@/components/NavBar";
 import ShopCard from "@/components/ShopCard";
 
+
+const images = [
+  "https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9?q=80&w=2706&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+'https://images.unsplash.com/photo-1473093295043-cdd812d0e601?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+'https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+
+];
 export default function Shop() {
+  const renderedCards = [];
+  for (let i = 1; i <= 40; i++) {
+    const image = i <= images.length ? images[i - 1] : undefined;
+    renderedCards.push(<ShopCard key={i} image={image} />);
+  }
 return (
 <>
 <Navbar></Navbar>
@@ -11,20 +24,7 @@ return (
     <div className="shop">
       <div className="filters mb-2 h-16 w-full border-4"></div>
       <div className="products">
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
-        <ShopCard></ShopCard>
+          {renderedCards}
       </div>
     </div>
 
--- src/components/ShopCard.tsx
+++ src/components/ShopCard.tsx
@@ -4,38 +4,43 @@ import "@/styles/shop-card.css";
 import { useState } from "react";
 
 const mockdata = {
-  image:"https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-  product_name: 'Pasta Sauce'
+  image:"https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9?q=80&w=2706&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  name: 'Pasta Sauce',
+  price: 24.99,
 };
 
+interface ShopCardProps {
+  image?: string
+}
 
-export default function ShopCard() {
-  const {image, product_name} = mockdata;
+export default function ShopCard(props: ShopCardProps) {
+  const { image = mockdata.image } = props;
+  const { name, price } = mockdata;
   const [count, setCount] = useState(0);
 
 return (
-    <article className="shop-card rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
+    <article className="shop-card max-w-lg rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
       <div className="relative flex items-end overflow-hidden rounded-xl">
-        <img src={image} alt="Hotel Photo" />
+        <img className="object-cover aspect-[4/3]" src={image} alt="Hotel Photo" />
       </div>
 
       <div className="text-section mt-1 p-2">
-        <h2 className="text-slate-700">{product_name}</h2>
-        <p className="mt-1 text-sm text-slate-400">Lisbon, Portugal</p>
+        <h2 className="text-slate-700 font-semibold">{name}</h2>
+        {/* <p className="mt-1 text-sm text-slate-400">Lisbon, Portugal</p> */}
 
         <div className="bot-section mt-3 flex items-end justify-between">
-            <p className="text-lg font-bold text-blue-500">$850</p>
+            <p className="text-lg font-bold text-black-500 leading-tight">RON {price}</p>
 
-        <div className="w-36 h-12">
+        <div className="">
           {count == 0 ? (
-            <div onClick={() => setCount(count+1)} className="w-36 h-12 flex justify-center items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
+            <button onClick={() => setCount(count+1)} className="flex justify-center items-center space-x-1.5 outline-none mr-1 mb-1 border border-solid border-red-500 rounded-full px-4 py-2 bg-transparent text-xs text-orange-500 font-bold uppercase focus:outline-none active:bg-orange-600 hover:bg-orange-600 hover:text-white">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-4 w-4">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
               </svg>
-                <button className="text-sm" >Add</button>
-            </div>
+                <p className="text-sm">Adauga</p>
+            </button>
           ) : (
-              <div className="max-w-xs mx-auto">
+              <div className="max-w-[6rem] max-h-[3rem] mx-auto">
                 <div className="relative flex items-center max-w-[8rem]">
                     <button onClick={() => setCount(count-1)} type="button" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
@@ -58,3 +63,5 @@ return (
   </article>
 );  
 }
+
+// outline-none mr-1 mb-1 border border-solid border-red-500 rounded-full px-4 py-2 bg-transparent text-xs text-red-500 font-bold uppercase focus:outline-none active:bg-red-600 hover:bg-red-600 hover:text-white
