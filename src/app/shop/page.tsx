909f1a82f1632770f38e3a1aa7238fcc0ab9d9e6
Eduard <eduard.bibac@protonmail.com>
2024-05-13 15:35:56 +0300

    ui: checkout button

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -6,8 +6,8 @@ import ShopCard from "@/components/ShopCard";
 const images = [
   "https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9?q=80&w=2706&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-'https://images.unsplash.com/photo-1473093295043-cdd812d0e601?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-'https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  'https://images.unsplash.com/photo-1473093295043-cdd812d0e601?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
 ];
 export default function Shop() {
@@ -29,7 +29,17 @@ return (
     </div>
 
     <div className="cart">
-      <div className="scroll-padding-top"></div>  
+      <div className="flex justify-between">
+      <p className="self-start text-xl py-5 font-bold dark:text-white">Total</p>
+      <p className="self-end text-xl py-5 font-bold dark:text-white pr-4">120.49 RON</p>
+    </div>
+
+
+    <button className="text-lg items-center w-full h-30 rounded-lg bg-orange-500 px-4 py-1.5 text-white duration-100 hover:bg-orange-600">Checkout</button>
+
+      <div className="scroll-padding-top">
+            
+      </div>  
       <div className="products">
         <ShopCard></ShopCard>
         <ShopCard></ShopCard>
@@ -48,7 +58,7 @@ return (
         <ShopCard></ShopCard>
       </div>
       
-      <button className="text-sm items-center w-full h-30 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">Checkout</button>
+
     </div>
     
 
--- src/components/ShopCard.tsx
+++ src/components/ShopCard.tsx
@@ -28,38 +28,40 @@ return (
         <h2 className="text-slate-700 font-semibold">{name}</h2>
         {/* <p className="mt-1 text-sm text-slate-400">Lisbon, Portugal</p> */}
 
-        <div className="bot-section mt-3 flex items-end justify-between">
-            <p className="text-lg font-bold text-black-500 leading-tight">RON {price}</p>
+<div className="bot-section mt-3 flex items-end justify-between">
+  <span className="split-text">
+    <p className="text-lg font-bold text-black-500 leading-tight">RON</p>
+    <p className="text-lg font-bold text-black-500 leading-tight">{price}</p>
+  </span>
 
-        <div className="">
-          {count == 0 ? (
-            <button onClick={() => setCount(count+1)} className="flex justify-center items-center space-x-1.5 outline-none mr-1 mb-1 border border-solid border-red-500 rounded-full lg:px-4 py-2 bg-transparent text-xs text-orange-500 font-bold uppercase focus:outline-none active:bg-orange-600 hover:bg-orange-600 hover:text-white">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-4 w-4">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-              </svg>
-                <p className="text-sm">Adauga</p>
-            </button>
-          ) : (
-              <div className="max-w-[6rem] max-h-[3rem] mx-auto">
-                <div className="relative flex items-center max-w-[8rem]">
-                    <button onClick={() => setCount(count-1)} type="button" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
-                        <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
-                        </svg>
-                    </button>
-                    <input value={count} type="text" id="quantity-input" data-input-counter aria-describedby="helper-text-explanation" className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required/>
-                    <button onClick={() => setCount(count+1)} type="button" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
-                        <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                        </svg>
-                    </button>
-                </div>
-              </div>
-          )}
-        </div>
-
-        </div>
+  <div className="w-full max-w-28">
+  {count === 0 ? (
+    <button onClick={() => setCount(count+1)} className="w-full flex justify-center items-center space-x-1.5 outline-none mr-1 mb-1 border border-solid border-red-500 rounded-full lg:px-4 md:px-2 sm:px-2 py-2 bg-transparent text-xs text-orange-500 font-bold uppercase focus:outline-none active:bg-orange-600 hover:bg-orange-600 hover:text-white">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-4 w-4">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+      </svg>
+      <p className="text-sm">Adauga</p>
+    </button>
+  ) : (
+    <div className="w-full flex justify-center">
+      <div className="relative flex items-center w-full">
+        <button onClick={() => setCount(count-1)} type="button" className="w-1/3 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+          <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="M1 1h16"/>
+          </svg>
+        </button>
+        <input value={count} type="text" id="quantity-input" data-input-counter aria-describedby="helper-text-explanation" className="w-1/3 bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required/>
+        <button onClick={() => setCount(count+1)} type="button" className="w-1/3 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+          <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+          </svg>
+        </button>
       </div>
+    </div>
+  )}
+</div>
+</div>
+</div>
   </article>
 );  
 }
