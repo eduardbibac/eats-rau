0afddd9eb5f17bbd07ce549dba3d883e9eb2702b
Eduard <eduard.bibac@protonmail.com>
2024-05-17 23:18:02 +0300

    chore: build issues

--- src/app/temp/page.tsx
+++ src/app/temp/page.tsx
@@ -1,25 +1,25 @@
-import TPill from "@/components/templates/TPill";
-import THeaderStatCards from "@/components/templates/THeaderStatCards";
-import TNavBar from "@/components/templates/TNavBar";
-import TTable from "@/components/templates/TTable";
-import TSearchBar from "@/components/templates/TSearchBar";
-import TBottomNavBar from "@/components/templates/TBottomNavBar/TBottomNavBar";
-import TStickyShopLayout from "@/components/templates/TStickyShopLayout/TStickyShopLayout";
-import Drawer from "@/components/Drawer";
+// import TPill from "@/components/templates/TPill";
+// import THeaderStatCards from "@/components/templates/THeaderStatCards";
+// import TNavBar from "@/components/templates/TNavBar";
+// import TTable from "@/components/templates/TTable";
+// import TSearchBar from "@/components/templates/TSearchBar";
+// import TBottomNavBar from "@/components/templates/TBottomNavBar/TBottomNavBar";
+// import TStickyShopLayout from "@/components/templates/TStickyShopLayout/TStickyShopLayout";
+// import Drawer from "@/components/templates/Drawer";
 
-export default function Temp() {
-  return (
-    <>
-      <TPill></TPill>
-      {/* <TNavBar></TNavBar> */}
-      {/* <THeaderStatCards></THeaderStatCards> */}
-      {/* <TTable></TTable> */}
-      {/* <TSearchBar></TSearchBar> */}
-      {/* <TBottomNavBar></TBottomNavBar> */}
+// export default function Temp() {
+//   return (
+//     <>
+//       <TPill></TPill>
+//       {/* <TNavBar></TNavBar> */}
+//       {/* <THeaderStatCards></THeaderStatCards> */}
+//       {/* <TTable></TTable> */}
+//       {/* <TSearchBar></TSearchBar> */}
+//       {/* <TBottomNavBar></TBottomNavBar> */}
 
-      {/* <TStickyShopLayout></TStickyShopLayout> */}
-      <Drawer></Drawer>
+//       {/* <TStickyShopLayout></TStickyShopLayout> */}
+//       <Drawer></Drawer>
 
-    </>
-  );
-}
+//     </>
+//   );
+// }
