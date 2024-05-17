e22034ca4016bb0565f2764859a42dac9aba9dde
Eduard <eduard.bibac@protonmail.com>
2024-05-17 23:25:03 +0300

    add redirect after qr code scan

--- src/app/login/qr/route.ts
+++ src/app/login/qr/route.ts
@@ -55,6 +55,9 @@ export async function GET(req: NextRequest) : Promise<Response> {
     
     return new Response(null, {
       status: 200,
+      headers: {
+        Location: "/"
+      }
     });
   }
 }
