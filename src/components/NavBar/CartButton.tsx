a5710d8c747684e0dda8ddd74f8051aea667172b
Eduard <eduard.bibac@protonmail.com>
2024-05-22 13:29:31 +0300

    ui: fix navbar felx

--- src/components/NavBar/CartButton.tsx
+++ src/components/NavBar/CartButton.tsx
@@ -5,17 +5,14 @@ import Cart from "../Cart";
 
 export default function CartButton() {
 return (
+  <div className="mb-active-text flex items-end justify-center text-center mx-auto px-4 pt-2 w-full text-gray-400 group-hover:text-orange-500">
   <Drawer>
       <DrawerTrigger>
-
-          <div className="mb-active-text flex items-end justify-center text-center mx-auto px-4 pt-2 w-full text-gray-400 group-hover:text-orange-500">
               <span className="block px-1 pt-1 pb-1">
                   <i className="far fa-shopping-cart text-2xl pt-1 mb-1 block"></i>
                   <span className="block text-xs pb-2">Cart</span>
                   <span className="mb-underline-active block w-5 mx-auto h-1 group-hover:bg-orange-500 rounded-full"></span>
               </span>
-          </div>
-
       </DrawerTrigger>
       <DrawerContent>
         <DrawerHeader>
@@ -25,6 +22,7 @@ return (
 
       </DrawerContent>
     </Drawer>
+    </div>
 
 );
 }
