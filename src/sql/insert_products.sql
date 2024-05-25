24d30670ca2bf68cae88151411d6b4ad0785f7b7
Eduard <eduard.bibac@protonmail.com>
2024-05-25 17:07:52 +0300

    insert products

---
+++ src/sql/insert_products.sql
@@ -0,0 +1,39 @@
+INSERT INTO product_categories(ro_name, en_name) VALUES 
+  ('Mic dejun', 'Breakfast'),
+  ('Prânz',     'Lunch'),
+  ('Cină',      'Dinner'),
+  ('Gustări',   'Snacks'),
+  ('Garnitură', 'Side Dish'),
+  ('Băuturi',   'Beverages'),
+  ('Vegan',     'Vegan'),
+  ('Porc',      'Pork'),
+  ('Vită',      'Beef'),
+  ('Pui',       'Chicken'),
+  ('Pește',     'Fish'),
+  ('Paste',     'Pasta')
+;
+
+INSERT INTO Products(ro_product_name, en_product_name, price, category, image_link) VALUES 
+  ('Cartofi prăjiț', 'French fries', 8.99, 4, 'https://images.unsplash.com/photo-1585109649139-366815a0d713')
+  ('Omletă cu legume', 'Vegetable Omelette', 12.99, 1, 'https://images.unsplash.com/photo-1510693206972-df098062cb71'),
+  ('Pancakes cu sirop de arțar', 'Pancakes with Maple Syrup', 15.99, 1, 'https://images.unsplash.com/photo-1586985288123-2495f577c250'),
+  ('Sandwich cu pui', 'Chicken Sandwich', 18.99, 2, 'chicken_sandwich.png'),
+  ('Salată Caesar', 'Caesar Salad', 14.99, 2, 'https://images.unsplash.com/photo-1550304943-4f24f54ddde9'),
+  ('Burger de vită', 'Beef Burger', 22.99, 3, 'https://unsplash.com/photos/double-patty-cheeseburger-jh5XyK4Rr3Y'),
+  ('Paste carbonara', 'Carbonara Pasta', 19.99, 3, 'https://unsplash.com/photos/a-white-plate-topped-with-a-pasta-dish-Ckka_jsngpM'),
+  ('Covrigei sărați', 'Salted Pretzels', 5.99, 4, 'https://unsplash.com/photos/a-group-of-sausages-from-a-string-GLkPxDqanD0'),
+  ('Nachos cu brânză', 'Cheese Nachos', 10.99, 4, 'https://unsplash.com/photos/white-and-red-ceramic-plate-DRs0juEZtsc'),
+  ('Piure de cartofi', 'Mashed Potatoes', 9.99, 5, 'https://unsplash.com/photos/yellow-cream-in-clear-glass-bowl-CJj2iQn6IsE'),
+  ('Legume la grătar', 'Grilled Vegetables', 11.99, 5, 'https://unsplash.com/photos/a-close-up-of-a-plate-of-food-with-meat-and-veggies--cLe54AcdYg'),
+  ('Limonadă', 'Lemonade', 6.99, 6, 'https://unsplash.com/photos/clear-drinking-glass-with-yellow-liquid-sSLqRCTJBvU'),
+  ('Smoothie de fructe', 'Fruit Smoothie', 8.99, 6, 'https://unsplash.com/photos/two-ice-cream-shakes-wBIeri0fLuw'),
+  ('Burger vegan', 'Vegan Burger', 59.99, 7, 'https://unsplash.com/photos/person-holding-meat-sandwich-with-sliced-tomatoes-onions-and-grilled-meat-pRKDJZWNUvY'),
+  ('Cotlet de porc', 'Pork Chop', 25.99, 8, 'https://unsplash.com/photos/grilled-steak-with-vegetables-on-white-ceramic-plate-Yr4n8O_3UPc'),
+  ('Coaste de porc BBQ', 'BBQ Pork Ribs', 27.99, 8, 'https://unsplash.com/photos/barbecue-rib-meal-with-cole-slaw-and-french-fries-shot-with-selective-focus-fvg00o9nZlY'),
+  ('Friptură de vită', 'Beef Steak', 29.99, 9, 'https://unsplash.com/photos/cooked-meat-on-tray-NYBnDWeOX2c'),
+  ('Piept de pui la grătar', 'Grilled Chicken Breast', 18.99, 10, 'https://unsplash.com/photos/a-plate-of-food-on-a-table-with-a-glass-of-wine-iVOjxsSxqnk'),
+  ('Aripioare de pui picante', 'Spicy Chicken Wings', 17.99, 10, 'https://unsplash.com/photos/a-plate-of-food-with-chopsticks-next-to-it-ako8aKb1_2c'),
+  ('Somon la cuptor', 'Baked Salmon', 26.99, 11, 'https://unsplash.com/photos/cooked-food-on-white-ceramic-plate-Iq0rbPBXJ8Y'),
+  ('Spaghete bolognese', 'Spaghetti Bolognese', 19.99, 12, 'https://unsplash.com/photos/spaghetti-and-meatballs-with-basil-garnish-shot-close-up-HTpiHBRoBIc'),
+;
+
