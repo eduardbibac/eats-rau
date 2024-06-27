450e6210e2dc8572cd1048a7975db3c17624a312
Eduard <eduard.bibac@protonmail.com>
2024-06-27 14:17:14 +0300

    increase db max connections

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,6 +3,7 @@ import postgres from "postgres";
 const sql = postgres(process.env.PG_CONNECTION_STRING!, {
   idle_timeout: 5,
   publications: "insert_orders",
+  max: 150,
 });
 
 export default sql;
